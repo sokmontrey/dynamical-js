76a5a6adf343adadc63cf78965e67127a8717f59
Sokmontrey <sythatsokmontrey@gmail.com>
2022-01-22 20:24:43 -0500

    implement polygon point collision with raycasting

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ const polygon = new Polygon([
 	{x: 0, y: -100}
 ]);
 
-
-
 /*
 function checkBorder(polygon, width, height){
 	var i;
@@ -57,36 +55,5 @@ function checkPoly(polygon1, polygon2){
 		if(collide) return true;
 	}
 	return false;
-}
-
-function checkCollision(point, polygon){
-	var i;
-	const vertices = polygon.vertices;
-	const p = {
-		x: point.x, 
-		y: point.y
-	};
-	var intersect = 0;
-
-	for(i=0; i<vertices.length; i++){
-		const a = {
-			x: vertices[i].x + polygon.position.x,
-			y: vertices[i].y + polygon.position.y
-		}
-		const next = vertices[i+1] || vertices[0]; 
-		const b = {
-			x: next.x + polygon.position.x,
-			y: next.y + polygon.position.y
-		}
-
-		if(p.y > Math.min(a.y, b.y) && p.y < Math.max(a.y, b.y) ){
-			if(p.x > Math.max(a.x, b.x)) continue;
-
-			//get x value from segment a and b by knowing by from p.y
-			const x = (p.y - a.y) * (b.x - a.x) / (b.y - a.y) + a.x;
-			if(x > p.x) intersect++;
-		}else continue;
-	}
-	return intersect % 2 === 1;
 }
 */
