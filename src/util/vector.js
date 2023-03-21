2d2b9fc32368a4121b3ce2918d8b80030d142b53
Sokmontrey <sythatsokmontrey@gmail.com>
2023-03-21 13:14:30 -0400

    add mirror method, and prototype methods Vector2

--- src/util/vector.js
+++ src/util/vector.js
@@ -3,6 +3,10 @@ function Vector2 (x=0, y=0){
     this.y = y;
 }
 
+/*
+Static Methods
+*/
+
 Vector2.add = (a, b)=>{
     if(b instanceof Vector2) return new Vector2(a.x + b.x, a.y+b.y);
     else return new Vector2(a.x+b, a.y+b);
@@ -44,9 +48,60 @@ Vector2.distance = (a,b)=>{
 Vector2.clone = (a)=>{
     return new Vector2(a.x, a.y);
 }
-/*
-Vector2.mirror = (a, normal_vector)=>{
-
+Vector2.mirror = (a, n)=>{
+    return Vector2.subtract(a,
+        Vector2.multiply(
+            2,
+            Vector2.multiply( 
+                n, 
+                -n.y*a.x + n.x*a.y
+            )
+        )
+    );
 }
 
+/*
+Instance Methods
 */
+
+Vector2.prototype = {
+    add: (other)=>{
+        return Vector2.add(this, other);
+    },
+    subtract: (other)=>{
+        return Vector2.subtract(this, other);
+    },
+    invert: ()=>{
+        return Vector2.invert(this);
+    },
+    multiply: (other)=>{
+        return Vector2.multiply(this, other);
+    },
+    divide: (other)=>{
+        return Vector2.divide(this, other);
+    },
+    dot: (other)=>{
+        return Vector2.dot(this, other);
+    },
+    magnitude: ()=>{
+        return Vector2.magnitude(this);
+    },
+    normalize: ()=>{
+        return Vector2.normalize(this);
+    },
+    min: ()=>{
+        return Vector2.min(this);
+    },
+    max: ()=>{
+        return Vector2.max(this);
+    },
+    distance: (other)=>{
+        return Vector2.distance(this, other);
+    },
+    clone: ()=>{
+        return Vector2.clone(this);
+    },
+    mirror: (n)=>{
+        return Vector2.mirror(this, n);
+    },
+};
