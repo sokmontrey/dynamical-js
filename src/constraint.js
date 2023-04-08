96f657b5f7ddc1af828819fb097976341e9d9e31
Sokmontrey <sythatsokmontrey@gmail.com>
2023-04-08 17:49:00 -0400

    refactoring constraint

--- src/constraint.js
+++ src/constraint.js
@@ -4,31 +4,43 @@ point: point mass
 p: positionition of point mass
 */
 
-//TODO: check Stress over Tension 
-
-export class DistanceConstraint{
-    constructor(points, 
-    {
-        record_tension=false
-    }={}){
+export class Constraint{
+    constructor({
+        points = []
+    }){
         this._points = points;
+    }
+
+    addPointMass(point){
+        this._points.push(point);
+    }
+
+    get points(){ return this._points; }
+}
+
+/* points: 2D array contain batchs of constraint. 
+* Each batch is the actual information indicating which points connecting together
+* */
+export class DistanceConstraint extends Constraint{
+    constructor(params){
+        super(params);
 
         this._distance = [];
-        for(let i=1; i<points.length; i++){
+        this._spring_constant = params.spring_constant || 1;
+        this._is_record_stress = params.record_stress || false;
+        this._stresses = [];
+
+        /* Setup equilabrium distance for each constraint */
+        for(let i=1; i< this._points.length; i++){
             this._distance.push(Vector2.distance(
-                points[i-1].position, points[i].position
+                this._points[i-1].position, this._points[i].position
             ));
         }
 
-        //TODO: change the spring constant from stiffness to sfiffness or something
-        this._spring_constant = 1;
-
-        this._is_record_tension = record_tension;
-        if(record_tension){
-            this._tensions = [];
-
+        /* Setup stress record */
+        if(this._is_record_stress){
             for(let i=0; i<points.length-1; i++){
-                this._tensions.push(0);
+                this._stresses.push(0);
             }
         }
     }
@@ -47,10 +59,10 @@ export class DistanceConstraint{
                 const difference_in_length = (l1-l2);
                 const error = Vector2.normalize(
                     p1.subtract(p2)
-                ).multiply(this._spring_constant * difference_in_length);
+                ).multiply( (1/this._spring_constant) * difference_in_length );
 
-                if(this._is_record_tension) {
-                    this._tensions[i-1] = this._tensions[i-1] + Math.abs(difference_in_length);
+                if(this._is_record_stress) {
+                    this._stresses[i-1] = this._stresses[i-1] + Math.abs(difference_in_length);
                 }
 
                 const m1_reciprocal = 1 / point1.mass;
@@ -72,85 +84,54 @@ export class DistanceConstraint{
         }
     }
 
-    get points() { return this._points; }
-
-    get tensions() { return this._tensions; }
+    get stresses() { return this._stresses; }
 
     getConstraint(){
         const result = [];
-        for(let i=0; i<this._tensions.length; i++){
+        for(let i=0; i<this._stresses.length; i++){
             result.push({
                 point1: this._points[i],
                 point2: this._points[i+1],
                 distance: this._distance[i],
-                tension: this._tensions[i],
+                stress: this._stresses[i],
             });
         }
-        this.resetTension();
+        this.resetStress();
         return result;
     }
 
-    resetTension(){
-        for(let i=0; i<this._tensions.length; i++){
-            this._tensions[i] = 0;
+    resetStress(){
+        for(let i=0; i<this._stresses.length; i++){
+            this._stresses[i] = 0;
         }
     }
 }
-export class SpringConstraint extends DistanceConstraint{
-    constructor(points, {spring_constant=0.04, record_tension=false}={}){
-        super(points, {record_tension: record_tension});
-        this._spring_constant = spring_constant;
-    }
-    setSpringConstant(new_spring_constant){
-        this._spring_constant = new_spring_constant;
-    }
-}
 
-export class ContainerConstraint{
-    constructor({
-        vertices=[],
-        points=[],
-    }){
-        this._points = points;
-        this._vertices = vertices;
-    }
+export class ContainerConstraint extends Constraint{
+    constructor(params){
+        super(params);
 
-    addPointMass(point){
-        this._points.push(point);
+        this._vertices = params.vertices || [];
     }
 
     check(){
-        /*
-            Use if statement outside the for loop to avoid
-            running if statement for every points
+        for(let i=0; i<this._points.length; i++){
+            const point = this._points[i];
+            const A = point.position;
+            const B = point.old_position;
 
-            Not using checkOnePoint method for the same reason
+            for(let i=1; i<this._vertices.length; i++){
+                this._check(point, A, B, 
+                    this._vertices[i-1], this._vertices[i]
+                );
+            }
 
-            redunduncy for small performance
-        */
-        for(let point_index=0; 
-            point_index<this._points.length; 
-            point_index++
-        ){
-
-            const point = this._points[point_index];
-            this.checkOnePoint(point);
-        }
-    }
-    checkOnePoint(point){
-        const A = point.position;
-        const B = point.old_position;
-
-        for(let j=1; j<this._vertices.length; j++){
             this._check(point, A, B, 
-                this._vertices[j-1], this._vertices[j]
+                this._vertices[this._vertices.length-1], this._vertices[0]
             );
         }
-
-        this._check(point, A, B, 
-            this._vertices[this._vertices.length-1], this._vertices[0]
-        );
     }
+
     _check(point, A, B, C, D){
         const contact_point = Vector2.getLineIntersection(A, B, C, D);
         const normal = new Vector2(C.y-D.y, D.x-C.x).normalize();
@@ -165,8 +146,8 @@ export class ContainerConstraint{
 
         //If the intersection is not even between the segment,
         //Don't bother
-        if(!Vector2.isPointBetweenSegment(contact_point, C, D)) 
-            return;
+        // if(!Vector2.isPointBetweenSegment(contact_point, C, D)) 
+        //     return;
 
         point.resolveCollision(contact_point, normal);
     }
@@ -189,7 +170,7 @@ export class BoxContainerConstraint extends ContainerConstraint{
             offset.add(new Vector2(w, 0)), 
             offset.add(new Vector2(w, h)), 
             offset.add(new Vector2(0, h))
-        ]
+        ];
 
         super(params);
     }
@@ -202,40 +183,21 @@ export class CircleContainerConstraint extends ContainerConstraint{
         this._offset = params.offset || new Vector2(0,0);
         this._center = params.offset.add(params.radius);
     }
-    checkOnePoint(point){
-        const A = point.position;
+    check(){
+        for(let i=0; i<this._points.length; i++){
+            const point = this._points[i];
+            const A = point.position;
 
-        const to_point = A.subtract(this._center);
-        const d = to_point.magnitude();
+            const to_point = A.subtract(this._center);
+            const d = to_point.magnitude();
 
-        if(d < this._radius) return;
+            if(d < this._radius) continue;
 
-        const to_point_normal = to_point.normalize();
-        const contact_point = to_point_normal.multiply(this._radius)
-            .add(this._center);
-        const normal = to_point_normal.invert();
-        point.resolveCollision(contact_point, normal);
+            const to_point_normal = to_point.normalize();
+            const contact_point = to_point_normal.multiply(this._radius).add(this._center);
+            const normal = to_point_normal.invert();
+
+            point.resolveCollision(contact_point, normal);
+        }
     }
 }
-
-// export class PointMassCollisionConstraint{
-//     constructor(){}
-//     check(point1, point2){
-//         const p1 = point1.position;
-//         const p2 = point2.position;
-//
-//         if(Vector2.distance(p1, p2) < point1.radius + point2.radius){
-//             const n1 = p2.subtract(p1).normalize();
-//             const n2 = n1.invert();
-//
-//             //TODO: finc contact_point for points with different radius
-//             const cp = p2.add(p1).divide(2);
-//
-//             const cp1 = cp.subtract(cp.subtract(p1).scaleMagnitudeTo(point1.radius));
-//             const cp2 = cp.subtract(cp.subtract(p2).scaleMagnitudeTo(point2.radius));
-//
-//             point1.resolveCollision(cp1, n1);
-//             point2.resolveCollision(cp2, n2);
-//         }
-//     }
-// }
