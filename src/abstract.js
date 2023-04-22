434bc2f724deb8792cee4b8290c5a185b4a9019b
Sokmontrey <sythatsokmontrey@gmail.com>
2023-04-22 11:35:10 -0400

    Create abstract.js

---
+++ src/abstract.js
@@ -0,0 +1,18 @@
+export default class Abstract{
+    constructor(){
+        this._name = null;
+        this._renderer = null;
+    }
+
+    setRenderer(renderer){
+        this._renderer = renderer;
+        return this;
+    }
+    setName(name){
+        this._name = name;
+        return this;
+    }
+    getName(){
+        return this._name;
+    }
+}
