3159e9ba19eb61ddaec011ead153b2c10fcd1697
Sokmontrey <sythatsokmontrey@gmail.com>
2023-05-12 22:22:12 -0400

    A little update

--- src/composite.js
+++ src/composite.js
@@ -7,73 +7,24 @@ export default class Composite extends Abstract{
     constructor(){
         super();
 
-        this._offset        = new Vector(0,0);
-        this._points        = {};
-        this._connections   = [];
-        this._container;
+        this._initial_offset    = new Vector(0,0);
+        this._points            = {};
     }
 
-    setOffset(vector){
-        this._offset = vector;
+    setOffset(offset){
+        this._initial_offset = offset;
+    }
+
+    addPointMass(point){
+        const name = point.getName() || Object.keys(this._points).length();
+        this._points[name] = point;
+
         return this;
     }
 
-    addPointMass(name, relative_position){
-        this._points[name] = new PointMass({
-            position: this._offset.add(relative_position)
-        })
-        this._container.addPointMass(this._points[name]);
+    createVertex(name=Object.keys(this._points).length(), vertex){
+        this._points[name] = PointMass.create(vertex);
+
         return this;
-    }
-
-    setContainer(container_constraint){
-        this._container = container_constraint;
-        return this;
-    }
-
-    connect(point1_name, point2_name){
-        this._connections.push(
-            new DistanceConstraint({})
-                .addPointMass(this._points[point1_name])
-                .addPointMass(this._points[point2_name])
-        );
-        return this;
-    }
-
-    draw(renderer){
-        for(let point_name in this._points){
-            const point = this._points[point_name];
-            renderer.circle({ 
-                position: point.position,
-                radius: 5
-            }).setFillStyle('white').fill();
-        }
-
-        for(let each of this._connections){
-            const point1 = each.points[0];
-            const point2 = each.points[1];
-            renderer.line({
-                start: point1.position,
-                end: point2.position,
-                line_width: 0.5
-            }).setStrokeStyle('white').stroke();
-        }
-    }
-
-    update(delta_time){
-        for(let point_name in this._points){
-            const point = this._points[point_name];
-            point.applyGravity();
-        }
-        for(let i=0; i<5; i++){
-            for(let each of this._connections){
-                each.check();
-            }
-            this._container.check();
-            for(let point_name in this._points){
-                const point = this._points[point_name];
-                point.updatePosition(delta_time / 2);
-            }
-        }
     }
 }
