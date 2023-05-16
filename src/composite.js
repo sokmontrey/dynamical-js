02608e814892f1eb7c454911d1ad5a5af56f79b2
Sokmontrey <sythatsokmontrey@gmail.com>
2023-05-15 21:10:41 -0400

    draw method in composite

--- src/composite.js
+++ src/composite.js
@@ -10,6 +10,21 @@ export default class Composite extends Abstract{
         this._initial_offset    = new Vector(0,0);
         this._points            = {};
         this._connections       = [];
+
+        this.graphic = {
+            ...this.graphic,
+            
+            is_fill: true,
+            is_wireframe: false,
+            is_vertex: false,
+
+            fill_color: 'white',
+            wireframe_color: 'gray',
+            vertex_color: '#aa3333',
+
+            wireframe_thickness: 1,
+            vertex_size: 3,
+        };
     }
 
     setOffset(offset){
@@ -44,6 +59,20 @@ export default class Composite extends Abstract{
         return this;
     }
 
+    setRenderer(renderer){
+        this.graphic.renderer = renderer;
+
+        for(let point_name in this._points){
+            this._points[point_name].setRenderer(renderer);
+        }
+
+        for(let i=0; i<this._connections.length; i++){
+            this._connections[i].setRenderer(renderer);
+        }
+
+        return this;
+    }
+
     update(delta_time){
         for(let point_name in this._points){
             this._points[point_name].updatePosition(delta_time);
@@ -70,20 +99,41 @@ export default class Composite extends Abstract{
     }
 
     draw(){
-        for(let connection of this._connections){
-            const points = connection.getPoints();
-            this._renderer.line({
-                start: points[0].position,
-                end: points[1].position,
-                line_width: 0.9
-            }).setStrokeStyle('gray').stroke();
+        const vertices = [];
+        if(this.graphic.is_fill){
+            for(let point_name in this._points){
+                vertices.push(this._points[point_name].position);
+            }
+
+            this.graphic.renderer.polygon({
+                vertices: vertices,
+            }).setFillStyle(this.graphic.fill_color).fill();
         }
 
-        for(let point_name in this._points){
-            this._renderer.circle({
-                position: this._points[point_name].position,
-                radius: 1,
-            }).setFillStyle('gray').fill();
+        if(this.graphic.is_wireframe){
+            for(let i=0; i<this._connections.length; i++){
+                const [point1, point2] = this._connections[i].getPoints();
+                this.graphic.renderer.line({
+                    start: point1.position,
+                    end: point2.position,
+                    line_width: this.graphic.wireframe_thickness,
+                }).setStrokeStyle(this.graphic.wireframe_color).stroke();
+            }
+        }
+
+        if(this.graphic.is_vertex){
+            if(!vertices.length){
+                for(let point_name in this._points){
+                    vertices.push(this._points[point_name].position);
+                }
+            }
+
+            for(let i=0; i<vertices.length; i++){
+                this.graphic.renderer.circle({
+                    position: vertices[i],
+                    radius: this.graphic.vertex_size,
+                }).setFillStyle(this.graphic.vertex_color).fill();
+            }
         }
 
         return this;
