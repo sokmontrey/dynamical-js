572fb17709af276d8421a463d1f2e24fcbf34c71
Sokmontrey <sythatsokmontrey@gmail.com>
2023-06-08 21:18:53 -0400

    create Graphic class

--- src/abstract.js
+++ src/abstract.js
@@ -1,30 +1,9 @@
+import { Graphic } from './renderer.js';
 export default class Abstract{
     constructor(){
         this.name       = null;
 
-        this.graphic    = {
-            renderer: null,
-
-            is_fill: 'true',
-            fill_style: 'white',
-            
-            is_stroke: false,
-            stroke_style: 'gray',
-            line_width: 2,
-
-            applyStyle: (shape)=>{
-                if(this.graphic.is_fill) {
-                    shape.setFillStyle(this.graphic.fill_style)
-                    .fill();
-                }
-
-                if(this.graphic.is_stroke) {
-                    shape.setLineWidth(this.graphic.line_width)
-                        .setStrokeStyle(this.graphic.stroke_style)
-                        .stroke();
-                }
-            }
-        }
+        this.graphic    = new Graphic();
     }
 
     setRenderer(renderer){
