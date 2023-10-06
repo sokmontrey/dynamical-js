b3c5c29be9de2aacceab7434b2c307a00120be74
Sokmontrey <sythatsokmontrey@gmail.com>
2023-10-06 13:26:40 -0400

    fix mouse_position relative to camera

--- src/util/input.js
+++ src/util/input.js
@@ -3,22 +3,39 @@ import { Vector } from './dynamical_vector.js';
 export default class Input{
     constructor(renderer){
         this._renderer = renderer;
-        this._control_axis = new Vector(null, null);
+
         this._mouse_position = new Vector(null, null);
-        this._mouse_event = {
-            "mousedown": false,
-            "mouseup": true,
-            "mousepress": ()=>{},
-        };
+        this._is_mousedown = false;
+        this._mouse_button = {};
     }
 
     listenMousePosition(){
-        const canvas = this._renderer.canvas;
+        const renderer = this._renderer;
+        const canvas = renderer.canvas;
         canvas.addEventListener('mousemove', (e)=>{
-            this._mouse_position.x = e.x - canvas.offsetLeft; 
-            this._mouse_position.y = e.y - canvas.offsetTop;
+            this._mouse_position.x = (e.x-canvas.offsetLeft) 
+                * renderer.camera.FOV.x 
+                + renderer.camera.position.x; 
+
+            this._mouse_position.y = (e.y-canvas.offsetTop) 
+                * renderer.camera.FOV.y 
+                + renderer.camera.position.y; 
         });
     }
+
+    listenMouseButton(){
+        const canvas = this._renderer.canvas;
+        canvas.addEventListener('mousedown', (e)=>{
+            this._is_mousedown = true;
+            this._mouse_button[e.buttons] = true;
+            this.onMousePress(e);
+        });
+        canvas.addEventListener('mouseup', (e)=>{
+            this._is_mousedown = false;
+            this._mouse_button = {};
+        });
+    }
+    onMousePress(){}
 
     getMouseX(){
         return this._mouse_position.x;
@@ -29,4 +46,7 @@ export default class Input{
     getMousePosition(){
         return this._mouse_position;
     }
+    get mouse_position() { return this._mouse_position; }
+    get is_mousedown() { return this._is_mousedown; }
+    get mouse_button() { return this._mouse_button; }
 }
