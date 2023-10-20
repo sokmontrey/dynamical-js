dc983153c066afba8e5ae4fa6554efbcd40df22e
Sokmontrey <sythatsokmontrey@gmail.com>
2023-10-20 14:07:27 -0400

    update entry points

--- src/collision/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Collider } from './collider.js';
-export { 
-    CircleCircleCollider,
-    PolygonCircleCollider,
-    PointPolygonCollider,
-    PointCircleCollider,
-} from './collider.js';
