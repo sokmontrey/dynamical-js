9114849b342400159b02b325a28bbfd649f68c40
Sokmontrey <sythatsokmontrey@gmail.com>
2023-12-24 14:50:08 -0500

    mass, lock, and step for addPosCorrection

--- src/dynamic/pointmass.js
+++ src/dynamic/pointmass.js
@@ -2,13 +2,23 @@ import Vector from "../math/vector.js";
 import Graphic from "../util/graphic.js";
 
 export default class PointMass {
-  constructor(x=250, y=250, mass=1) {
+  constructor(x = 250, y = 250, mass = 1) {
     this.old_pos = new Vector(x, y);
     this.now_pos = new Vector(x, y);
     this.pos_correction = new Vector(0, 0);
     this.acc = new Vector(0, 0);
     this.mass = mass;
+    this.is_locked = false;
+
     this.graphic = new Graphic("gray", "black");
+  }
+
+  lock() {
+    this.is_locked = true;
+    return this;
+  }
+  isLocked() {
+    return this.is_locked;
   }
 
   applyForce(force) {
@@ -16,14 +26,22 @@ export default class PointMass {
     this.acc = this.acc.add(force.div(this.mass));
   }
 
-  updatePosition(dt = 0.025, ) {
-    if (!dt) console.error("PointMass: updatePosition: dt is undefined or zero");
+  updatePosition(dt = 0.025) {
+    if (this.is_locked) {
+      console.error("PointMass: calling updatePosition on static pointmass");
+      return;
+    }
+    if (!dt) {
+      console.error("PointMass: updatePosition: dt is undefined or zero");
+      return;
+    }
 
-    const temp_pos = this.now_pos.add(this.pos_correction);
-    this.now_pos = temp_pos
-      .add(this.now_pos.sub(this.old_pos))
+    const temp = this.now_pos.add(this.pos_correction);
+    this.now_pos = temp
+      .add(temp.sub(this.old_pos))
       .add(this.acc.mul(dt * dt));
-    this.old_pos = temp_pos;
+
+    this.old_pos = temp;
     this.pos_correction = new Vector(0, 0);
   }
 
@@ -32,12 +50,12 @@ export default class PointMass {
   }
   set position(pos) {
     Vector.checkNan(pos, "PointMass.set position");
-    this.now_pos= pos;
+    this.now_pos = pos;
   }
 
-  addPosCorrection(correction) {
+  addPosCorrection(correction, step=1) {
     Vector.checkNan(correction, "PointMass.addPositionCorrection");
-    this.pos_correction = this.pos_correction.add(correction);
+    this.pos_correction = this.pos_correction.add(correction.div(step));
   }
 
   get old_position() {
@@ -46,7 +64,7 @@ export default class PointMass {
   set old_position(pos) {
     Vector.checkNan(pos, "PointMass.set old_position");
     this.old_pos = pos;
-  } 
+  }
 
   get velocity() {
     return this.now_pos.sub(this.old_pos);
@@ -54,5 +72,13 @@ export default class PointMass {
   set velocity(vel) {
     Vector.checkNan(vel, "PointMass.set velocity");
     this.old_pos = this.now_pos.sub(vel);
+  }
+
+  getMass() {
+    return this.mass;
+  }
+  setMass(mass){
+    this.mass = mass;
+    return this;
   }
 }
