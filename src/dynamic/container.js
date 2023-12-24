da225f9d7b52b40484a7e207a8e16ae619a5e4a1
Sokmontrey <sythatsokmontrey@gmail.com>
2023-12-24 12:07:04 -0500

    drawable container

--- src/dynamic/container.js
+++ src/dynamic/container.js
@@ -1,4 +1,5 @@
 import Vector from "../math/vector.js";
+import Graphic from "../util/graphic.js";
 
 export default class Container {
   constructor(points = [], x = 0, y = 0, w = 500, h = 500) {
@@ -6,6 +7,11 @@ export default class Container {
     this.corner = new Vector(x + w, y + h);
 
     this.points = points;
+    this.graphic = new Graphic("white", "gray")
+      .setStrokeWidth(2)
+      .setStrokeColor("gray")
+      .noFill()
+      .stroke();
   }
 
   addPoint(point) {
