517548b94aefaf116db5fa04d8e2c227312e24ac
Sokmontrey <sythatsokmontrey@gmail.com>
2024-10-19 19:29:01 -0400

    create: factory methods

--- src/utils/Vector.ts
+++ src/utils/Vector.ts
@@ -6,4 +6,15 @@ export default class Vec2 {
 		this.x = x;
 		this.y = y;
 	}
+	static zero(): Vec2 {
+		return new Vec2(0, 0);
+	}
+	static one(): Vec2 {
+		return new Vec2(1, 1);
+	}
+	static fromArray(arr: number[]): Vec2 {
+		if (arr.length != 2)
+			throw new Error(`Invalid array size (required 2 elements)`);
+		return new Vec2(arr[0], arr[1]);
+	}
 }
