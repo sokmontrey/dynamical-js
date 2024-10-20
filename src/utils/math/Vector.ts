eeafd79062baf3ec33e3816bc248e21e5f1ecfd1
Sokmontrey <sythatsokmontrey@gmail.com>
2024-10-20 19:14:36 -0400

    rearrage + add math method

--- src/utils/math/Vector.ts
+++ src/utils/math/Vector.ts
@@ -1,7 +1,4 @@
 export default class Vec2 {
-    mag(): number {
-		return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
 	x: number = 0;
 	y: number = 0;
 
@@ -10,35 +7,33 @@ export default class Vec2 {
 		this.y = y;
 	}
 
-	add(other: Vec2): Vec2 {
-		return new Vec2(this.x + other.x, this.y + other.y);
+	add(other: Vec2): Vec2 { return new Vec2(this.x + other.x, this.y + other.y); }
+	sub(other: Vec2): Vec2 { return new Vec2(this.x - other.x, this.y - other.y); }
+	scale(other: number): Vec2 { return new Vec2(this.x * other, this.y * other); }
+	mag(): number { return Math.sqrt(this.x * this.x + this.y * this.y); }
+
+	div(other: number): Vec2 {
+		if (!other) throw new Error("Cannot divide by zero");
+		return new Vec2(this.x / other, this.y / other);
+	}
+	eleDiv(other: Vec2): Vec2 {
+		if (!other.x || !other.y) throw new Error("Cannot divide by zero");
+		return new Vec2(this.x / other.x, this.y / other.y);
 	}
 
-	sub(other: Vec2): Vec2 {
-		return new Vec2(this.x - other.x, this.y - other.y);
-	}
+	distance(other: Vec2): number { return this.sub(other).mag(); }
+	invert(): Vec2 { return new Vec2(-this.x, -this.y); }
+	norm(): Vec2 { return this.div(this.mag()); }
+	toArray() { return [this.x, this.y]; }
+	copy(): Vec2 { return new Vec2(this.x, this.y); }
 
-	scale(other: number): Vec2 {
-		return new Vec2(this.x * other, this.y * other);
-	}
+	static right(length: number = 1): Vec2 { return new Vec2(length, 0); }
+	static left(length: number = 1): Vec2 { return new Vec2(-length, 0); }
+	static up(length: number = 1): Vec2 { return new Vec2(0, -length); }
+	static down(length: number = 1): Vec2 { return new Vec2(0, length); }
+	static zero(): Vec2 { return new Vec2(0, 0); }
+	static one(): Vec2 { return new Vec2(1, 1); }
 
-	toArray() {
-		return [this.x, this.y];
-	}
-
-	copy(): Vec2 {
-		return new Vec2(this.x, this.y);
-	}
-
-	static zero(): Vec2 {
-		return new Vec2(0, 0);
-	}
-	static one(): Vec2 {
-		return new Vec2(1, 1);
-	}
-	static fromXY(x: number, y: number): Vec2 {
-		return new Vec2(x, y);
-	}
 	static fromPolar(theta: number, r: number): Vec2 {
 		return new Vec2(r * Math.cos(theta), r * Math.sin(theta));
 	}
