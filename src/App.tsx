2bd844604ded3dc422da4807d049adb9aa047b88
Sokmontrey <sythatsokmontrey@gmail.com>
2024-10-24 12:51:38 -0400

    create: App simple canvas setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
+import Canvas from "./interfaces/Canvas";
+import PointMass from "./dynamics/PointMass";
+import Vec2 from "./utils/math/Vector";
 
-function App() {
+export default function App() {
+	const canvas_ref = useRef(null);
+
+	useEffect(() => {
+		if (!canvas_ref.current) return;
+		const canvas = new Canvas(canvas_ref.current, {
+			width: window.innerWidth,
+			height: window.innerHeight
+		});
+
+		const update = (dt: number) => {
+			dt *= 10;
+			canvas.clear();
+		};
+
+		let animation_frame_id: number;
+		let prev_time = 0;
+		const _render = (current_time: number) => {
+			const dt = Math.min((current_time - prev_time) / 1000, 0.016);
+			update(dt);
+			prev_time = current_time;
+			animation_frame_id = window.requestAnimationFrame(_render);
+		};
+		window.requestAnimationFrame(_render);
+		return () => window.cancelAnimationFrame(animation_frame_id);
+	}, [canvas_ref]);
+
 	return (
-		<h1>Hello world</h1>
+		<canvas ref={canvas_ref} ></canvas>
 	);
 }
-
-export default App
