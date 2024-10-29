88d035ac66e3c6741faf75eab37c043f0fd5db08
Sokmontrey <sythatsokmontrey@gmail.com>
2024-10-28 22:08:16 -0400

    unessary

--- src/utils/FP/Result.ts
+++
@@ -1,9 +0,0 @@
-export type Success<T> = { ok: true, value: T };
-export type Failure = { ok: false, err_msg: string };
-
-export const success =
-	<T>(value: T): Success<T> => ({ ok: true, value });
-export const failure =
-	(err_msg: string): Failure => ({ ok: false, err_msg });
-
-export type Result<T> = Success<T> | Failure;
