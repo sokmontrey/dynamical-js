6d22833b48e89dc9903066bf590a3d088247e594
Sokmontrey <sythatsokmontrey@gmail.com>
2024-10-29 22:26:43 -0400

    create: translation methods

--- src/utils/math/Vector.ts
+++ src/utils/math/Vector.ts
@@ -14,7 +14,7 @@ export default class Vec2 {
 	mul(other: number) { return vec2(this.x * other, this.y * other); }
 	mag() { return Math.sqrt(this.x * this.x + this.y * this.y); }
 
-	div(other: number) { 
+	div(other: number) {
 		if (!other) throw new Error("Cannot divide by zero");
 		return new Vec2(this.x / other, this.y / other);
 	}
@@ -24,6 +24,11 @@ export default class Vec2 {
 	norm() { return this.div(this.mag()); }
 	toArray() { return [this.x, this.y]; }
 	copy() { return vec2(this.x, this.y); }
+
+	right(length: number = 1) { return this.add(vec2(length, 0)); }
+	left(length: number = 1) { return this.add(vec2(-length, 0)); }
+	up(length: number = 1) { return this.add(vec2(0, -length)); }
+	down(length: number = 1) { return this.add(vec2(0, length)); }
 
 	static right(length: number = 1) { return vec2(length, 0); }
 	static left(length: number = 1) { return vec2(-length, 0); }
