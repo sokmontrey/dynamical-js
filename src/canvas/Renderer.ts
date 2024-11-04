6e43f94775c016a1029ccd4827526b405f74d864
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-03 23:07:35 -0500

    impl: Renderer constructor

--- src/canvas/Renderer.ts
+++ src/canvas/Renderer.ts
@@ -13,6 +13,21 @@ export default class Renderer {
 	protected stroke_color: string;
 	protected line_width: number;
 
+	constructor({
+		is_fill = true,
+		is_stroke = false,
+		fill_color = "blue",
+		stroke_color = "gray",
+		line_width = 1,
+	}: RendererParams = {}) {
+
+		this.is_fill = is_fill;
+		this.is_stroke = is_stroke;
+		this.fill_color = fill_color;
+		this.stroke_color = stroke_color;
+		this.line_width = line_width;
+	}
+
 }
 
 }
