cba1b5b6f3f9f6e33b7c20914539304c407dd3de
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-03 22:29:14 -0500

    create: PointMass class + private attributes

--- src/dynamics/PointMass.ts
+++ src/dynamics/PointMass.ts
@@ -9,3 +9,12 @@ export interface PointMassParams {
 	is_static?: boolean,
 };
 
+export default class PointMass {
+	private curr_pos: Vec2;
+	private prev_pos: Vec2;
+	private const_acc: Vec2;
+	private net_force: Vec2;
+	private mass: number;
+	private is_static: boolean;
+
+}
