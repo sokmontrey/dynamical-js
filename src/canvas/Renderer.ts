c3fb672e3d1a352cf0658f76b67cc51b0e5c1ff6
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-03 23:07:18 -0500

    create: Renderer class

--- src/canvas/Renderer.ts
+++ src/canvas/Renderer.ts
@@ -6,3 +6,13 @@ export interface RendererParams {
 	line_width?: number;
 };
 
+export default class Renderer {
+	protected is_fill: boolean;
+	protected is_stroke: boolean;
+	protected fill_color: string;
+	protected stroke_color: string;
+	protected line_width: number;
+
+}
+
+}
