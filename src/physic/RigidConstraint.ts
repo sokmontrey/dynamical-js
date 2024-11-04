bd66dbaa95696c7304ceede98f2b897284cd83b1
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-04 16:03:23 -0500

    impl: update

--- src/physic/RigidConstraint.ts
+++ src/physic/RigidConstraint.ts
@@ -6,6 +6,8 @@ export default class RigidConstraint {
 	protected pm1: PointMass;
 	protected pm2: PointMass;
 	protected rest_distance: number = 0;
+	protected diff: number;
+	protected corr: number;
 
 	constructor(pointmass1: PointMass,
 		pointmass2: PointMass,
@@ -14,6 +16,8 @@ export default class RigidConstraint {
 		this.pm1 = pointmass1;
 		this.pm2 = pointmass2;
 		this.calculateRestDistance();
+		this.diff = 0;
+		this.corr = 0;
 	}
 
 	calculateRestDistance() {
@@ -23,4 +27,25 @@ export default class RigidConstraint {
 		return this;
 	}
 
+	update(_: number) {
+		const pos1 = this.pm1.getPosition();
+		const pos2 = this.pm2.getPosition();
+		const curr_distance = pos1.distance(pos2);
+		this.diff = this.rest_distance - curr_distance;
+
+		if (Math.abs(this.diff) < 1e-9) return this;
+
+		this.corr = this.diff / this.rest_distance; // TODO: make sure rest_distance is not zero
+		const v = pos2.sub(pos1);
+
+		const mass1 = this.pm1.isStatic() ? 0 : 1 / this.pm1.getMass();
+		const mass2 = this.pm2.isStatic() ? 0 : 1 / this.pm2.getMass();
+		const total_mass = (mass1 + mass2);
+
+		const new_pos1 = pos1.sub(v.mul(this.corr * mass1 / total_mass));
+		const new_pos2 = pos2.add(v.mul(this.corr * mass2 / total_mass));
+
+		if(!this.pm1.isStatic()) this.pm1.setPosition(new_pos1);
+		if(!this.pm2.isStatic()) this.pm2.setPosition(new_pos2);
+	}
 }
