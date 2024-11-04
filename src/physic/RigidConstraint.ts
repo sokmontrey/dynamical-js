6a40122f40d6ada5fbb32a820859c16a5951f33a
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-04 16:02:56 -0500

    rename: pointmass -> pm

--- src/physic/RigidConstraint.ts
+++ src/physic/RigidConstraint.ts
@@ -3,23 +3,24 @@ import PointMass from "./PointMass";
 export interface RigidConstraintParams { };
 
 export default class RigidConstraint {
-	protected pointmass1: PointMass;
-	protected pointmass2: PointMass;
+	protected pm1: PointMass;
+	protected pm2: PointMass;
 	protected rest_distance: number = 0;
 
-	constructor(pointmass1: PointMass, 
-				pointmass2: PointMass,
-	{ }: RigidConstraintParams = {}) {
+	constructor(pointmass1: PointMass,
+		pointmass2: PointMass,
+		{ }: RigidConstraintParams = {}) {
 
-		this.pointmass1 = pointmass1;
-		this.pointmass2 = pointmass2;
+		this.pm1 = pointmass1;
+		this.pm2 = pointmass2;
 		this.calculateRestDistance();
 	}
 
 	calculateRestDistance() {
-		const pos1 = this.pointmass1.getPosition();
-		const pos2 = this.pointmass2.getPosition();
+		const pos1 = this.pm1.getPosition();
+		const pos2 = this.pm2.getPosition();
 		this.rest_distance = pos1.distance(pos2);
 		return this;
 	}
+
 }
