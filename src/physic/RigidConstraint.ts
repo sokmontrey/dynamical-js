4bad6998c4ecbfb33e76e44fe89e05d6fcfaabc3
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-04 10:28:56 -0500

    create: RigidConstraint class + protected attributes

---
+++ src/physic/RigidConstraint.ts
@@ -0,0 +1,8 @@
+import PointMass from "./PointMass";
+
+export default class RigidConstraint {
+	protected pointmass1: PointMass;
+	protected pointmass2: PointMass;
+	protected rest_distance: number = 0;
+
+}
