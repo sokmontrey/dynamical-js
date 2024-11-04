3c069e9b80bb9afb56a10b7abeb6726bebac5aa3
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-04 10:29:15 -0500

    impl: calculateRestDistance()

--- src/physic/RigidConstraint.ts
+++ src/physic/RigidConstraint.ts
@@ -5,4 +5,11 @@ export default class RigidConstraint {
 	protected pointmass2: PointMass;
 	protected rest_distance: number = 0;
 
+
+	calculateRestDistance() {
+		const pos1 = this.pointmass1.getPosition();
+		const pos2 = this.pointmass2.getPosition();
+		this.rest_distance = pos1.distance(pos2);
+		return this;
+	}
 }
