17a60c0cb90b085b85d4a6f181fdb1670aff99bf
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-05 21:27:05 -0500

    impl: is_enable getter

--- src/shape/Shape.ts
+++ src/shape/Shape.ts
@@ -2,5 +2,6 @@ export default class Shape {
 	protected is_enable = true;
 	enable() { this.is_enable = true; return this; }
 	disable() { this.is_enable = false; return this; }
+	isEnable() { return this.is_enable; }
 	applyStyleToContext(_: CanvasRenderingContext2D): void { }
 };
