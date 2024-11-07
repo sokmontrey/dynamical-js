353256e272e476d9ca6528589c2fbc692c52d8c8
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-07 12:52:44 -0500

    rename: toString -> toRGBA

--- src/utils/color/Color.ts
+++ src/utils/color/Color.ts
@@ -15,7 +15,7 @@ export default class Color {
 		this.a = a;
 	}
 
-	toString() { return `rgba(${this.r},${this.g},${this.b},${this.a})`; }
+	toRGBA() { return `rgba(${this.r},${this.g},${this.b},${this.a})`; }
 
 	/**
 	*	Linear interpolation between two colors
