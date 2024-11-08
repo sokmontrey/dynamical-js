22d60479990c7f95e27d0c5676afefe1ce8d4da8
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-07 22:32:29 -0500

    change: pm -> pointmas, impl: getter, add: comments

--- src/core-physic/RigidConstraint.ts
+++ src/core-physic/RigidConstraint.ts
@@ -1,52 +1,59 @@
 import RigidConstraintRenderer from "../renderer/RigidConstraintRenderer";
 import PointMass from "./PointMass";
 
-export interface RigidConstraintParams { };
 export interface RigidConstraintParams { 
 	is_broken?: boolean;
 };
 
 export default class RigidConstraint {
-	protected pm1: PointMass;
-	protected pm2: PointMass;
+	protected pointmass1: PointMass;
+	protected pointmass2: PointMass;
 	protected is_broken: boolean;
 
 	protected rest_distance: number = 0;
 	protected diff: number;
 	protected corr: number;
 
-	public readonly renderer: RigidConstraintRenderer;
+	public readonly renderer: RigidConstraintRenderer = new RigidConstraintRenderer(this);
 
 	constructor(pointmass1: PointMass, pointmass2: PointMass, {
 		is_broken = false,
 	}: RigidConstraintParams = {}) {
 
-		this.pm1 = pointmass1;
-		this.pm2 = pointmass2;
+		this.pointmass1 = pointmass1;
+		this.pointmass2 = pointmass2;
 		this.is_broken = is_broken;
 		this.calculateRestDistance();
 		this.diff = 0;
 		this.corr = 0;
-
-		this.renderer = new RigidConstraintRenderer(this);
 	}
 
+	//================================ Helpers ================================
+	
 	calculateRestDistance() {
-		const pos1 = this.pm1.getPosition();
-		const pos2 = this.pm2.getPosition();
+		const pos1 = this.pointmass1.getPosition();
+		const pos2 = this.pointmass2.getPosition();
 		this.rest_distance = pos1.distance(pos2);
 		if (this.rest_distance === 0)
 			throw new Error("Rigid constraint cannot have rest distance = 0. Please use Hinge constraint instead.");
 		return this;
 	}
 
+	//================================ Getters ================================
+
+	/**
+	*	@return [pointmass1, pointmass2]
+	**/
 	getPointMasses() {
-		return [this.pm1, this.pm2];
+		return [this.pointmass1, this.pointmass2];
 	}
 
 	/**
 	*	The difference between rest distance and current distance
+	*		calculated after .update(dt)
 	**/
+	getDifference() {
+		return this.diff;
 	}
 
 	/**
@@ -99,9 +106,9 @@ export default class RigidConstraint {
 	*			(temp_pos += corr_pos; count ++; temp_pos / corr_pos)
 	**/
 	update(_: number) {
-		const pos1 = this.pm1.getPosition();
-		const pos2 = this.pm2.getPosition();
 		if (this.is_broken) return this;
+		const pos1 = this.pointmass1.getPosition();
+		const pos2 = this.pointmass2.getPosition();
 		const curr_distance = pos1.distance(pos2);
 		this.diff = this.rest_distance - curr_distance;
 
@@ -109,17 +116,17 @@ export default class RigidConstraint {
 		if (this.rest_distance === 0)
 			throw new Error("Rigid constraint cannot have rest distance = 0. Please use Hinge constraint instead.");
 
-		this.corr = this.diff / this.rest_distance; // TODO: make sure rest_distance is not zero
+		this.corr = this.diff / this.rest_distance;
 		const v = pos2.sub(pos1);
 
-		const mass1 = this.pm1.isStatic() ? 0 : 1 / this.pm1.getMass();
-		const mass2 = this.pm2.isStatic() ? 0 : 1 / this.pm2.getMass();
+		const mass1 = this.pointmass1.isStatic() ? 0 : 1 / this.pointmass1.getMass();
+		const mass2 = this.pointmass2.isStatic() ? 0 : 1 / this.pointmass2.getMass();
 		const total_mass = (mass1 + mass2);
 
 		const new_pos1 = pos1.sub(v.mul(this.corr * mass1 / total_mass));
 		const new_pos2 = pos2.add(v.mul(this.corr * mass2 / total_mass));
 
-		if (!this.pm1.isStatic()) this.pm1.setCurrentPosition(new_pos1);
-		if (!this.pm2.isStatic()) this.pm2.setCurrentPosition(new_pos2);
+		if (!this.pointmass1.isStatic()) this.pointmass1.setCurrentPosition(new_pos1);
+		if (!this.pointmass2.isStatic()) this.pointmass2.setCurrentPosition(new_pos2);
 	}
 }
