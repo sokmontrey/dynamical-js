70fcf9f5e48e3d3e3514ed0b108c83a4b0076b23
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-07 22:42:01 -0500

    add: setters, getters, comments

--- src/core-physic/PointMass.ts
+++ src/core-physic/PointMass.ts
@@ -28,35 +28,63 @@ export default class PointMass {
 		constant_acceleration = Vec2.zero(),
 		initial_force = Vec2.zero(),
 	}: PointMassParams = {}) {
-		this.curr_pos = position;
+		this.curr_pos = position.copy();
 		this.prev_pos = position.sub(velocity);
 		this.mass = mass;
 		this.is_static = is_static;
-		this.net_force = initial_force;
-		this.const_acc = constant_acceleration;
+		this.net_force = initial_force.copy();
+		this.const_acc = constant_acceleration.copy();
 
 		this.renderer = new PointMassRenderer(this);
 	}
 
-	// ------------------------------ getters ------------------------------
+	//================================ Getters ================================
 
 	getTotalAcceleration() {
 		return this.net_force
 			.div(this.mass)			//	 net force / mass
 			.add(this.const_acc);	// + constant acceleration
 	}
-	getPosition() { return this.curr_pos; }
-	getPreviousPosition() { return this.prev_pos; }
-	getVelocity() { return this.curr_pos.sub(this.prev_pos); }
-	getMass() { return this.mass; }
-	isStatic() { return this.is_static; }
-	getNetForce() { return this.net_force; }
-	getConstantAcceleration() { return this.const_acc; }
 
-	// ------------------------------ setters ------------------------------
+	getPosition() {
+		return this.curr_pos;
+	}
 
-	enableStatic() { this.is_static = true; return this; }
-	disableStatic() { this.is_static = false; return this; }
+	getPreviousPosition() {
+		return this.prev_pos;
+	}
+
+	getVelocity() {
+		return this.curr_pos.sub(this.prev_pos);
+	}
+
+	getMass() {
+		return this.mass;
+	}
+
+	isStatic() {
+		return this.is_static;
+	}
+
+	getNetForce() {
+		return this.net_force;
+	}
+
+	getConstantAcceleration() {
+		return this.const_acc;
+	}
+
+	//================================ Setters ================================
+
+	enableStatic() {
+		this.is_static = true; 
+		return this;
+	}
+
+	disableStatic() {
+		this.is_static = false; 
+		return this;
+	}
 
 	applyForce(force: Vec2) {
 		this.net_force = this.net_force.add(force);
@@ -64,7 +92,7 @@ export default class PointMass {
 	}
 
 	setConstantAcceleration(acceleration: Vec2) {
-		this.const_acc = acceleration;
+		this.const_acc = acceleration.copy();
 		return this;
 	}
 
@@ -74,7 +102,7 @@ export default class PointMass {
 	**/
 	changePosition(position: Vec2) {
 		const vel = this.curr_pos.sub(this.prev_pos);
-		this.curr_pos = position;
+		this.curr_pos = position.copy();
 		this.prev_pos = position.sub(vel);
 		return this;
 	}
@@ -84,27 +112,40 @@ export default class PointMass {
 	*	allowing the pointmass to response to the interaction (position-based dynamic)
 	**/
 	setCurrentPosition(position: Vec2) {
-		this.curr_pos = position;
+		this.curr_pos = position.copy();
 		return this;
 	}
 
 	setPreviousPosition(previous_position: Vec2) {
-		this.prev_pos = previous_position;
+		this.prev_pos = previous_position.copy();
 		return this;
 	}
- 
+
 	/**
 	*	Update both current and previous position.
 	*	The velocity become zero
 	**/
 	setPosition(position: Vec2) {
-		this.curr_pos = position;
-		this.prev_pos = position;
+		this.curr_pos = position.copy();
+		this.prev_pos = position.copy();
 		return this;
 	}
- 
+
+	/**
+	*	Override previous position to assign new velocity to the pointmass
+	**/
 	setVelocity(velocity: Vec2) {
 		this.prev_pos = this.curr_pos.sub(velocity);
+		return this;
+	}
+
+	addVelocity(velocity: Vec2) {
+		this.prev_pos = this.prev_pos.sub(velocity);
+		return this;
+	}
+
+	resetVelocity() {
+		this.prev_pos = this.curr_pos.copy();
 		return this;
 	}
 
@@ -113,8 +154,11 @@ export default class PointMass {
 		return this;
 	}
 
+	//================================ Dynamic ================================
+
 	/**
-	*	Position-based dynamic (verlet integration)
+	*	Update current position based on total accelration and previous position 
+	*		enabling position-based dynamic (verlet integration).
 	**/
 	update(delta_time: number) {
 		if (this.is_static) return this;
