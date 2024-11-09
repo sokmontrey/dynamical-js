a88a32378ccba56d78b666f3429626bbb2f4a601
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-08 22:51:03 -0500

    basic loop manager functionalities (run, stop, step, restart)

--- src/core/Loop.ts
+++ src/core/Loop.ts
@@ -3,41 +3,58 @@ export interface LoopParams {
 	sub_steps?: number,
 }
 
-export type update_func = (dt: number, steps: number) => {}
+export type update_func = (dt: number) => {}
 
 export default class Loop {
 	private is_constant_dt: boolean;
-	private constant_dt: number; 
-	private sub_steps: number;
+	private constant_dt: number;
+
+	private prev_time: number;
 	private update_func: update_func;
+	private frame_id: number | null;
+	private frame_count: number;
 
 	constructor(update_func: update_func, {
 		constant_dt = null,
-		sub_steps = 1,
 	}: LoopParams = {}) {
 		this.update_func = update_func;
 		this.is_constant_dt = constant_dt !== null;
-		this.constant_dt = constant_dt ?? 0.016;
-		this.sub_steps = sub_steps;
+		this.constant_dt = constant_dt ?? 0.16;
+		this.prev_time = 0;
+		this.frame_id = null;
+		this.frame_count = 0;
 	}
 
-	run() {
+	private getDeltaTime(current_time: number) {
+		if (this.is_constant_dt) return this.constant_dt;
+		const dt = Math.min((current_time - this.prev_time) / 10, 0.16);
+		this.prev_time = current_time;
+		return dt;
+	}
 
+	_update(current_time: number) {
+		const dt = this.getDeltaTime(current_time);
+		this.frame_count++;
+		this.update_func(dt);
+		this.frame_id = window.requestAnimationFrame(this._update);
+	}
+
+	run(): number {
+		this.frame_id = window.requestAnimationFrame(this._update);
+		return this.frame_id;
 	}
 
 	pause() {
-
-	}
-
-	stop() {
-
+		if (this.frame_id === null) return;
+		window.cancelAnimationFrame(this.frame_id);
 	}
 
 	restart() {
-
+		this.frame_count = 0;
 	}
 
-	step(step_size: number) {
-
+	step(dt: number) {
+		this.frame_count ++;
+		this.update_func(dt);
 	}
 }
