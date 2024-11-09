5fed73f009c7ca214fdbeec447586b10efc17928
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-08 23:12:27 -0500

    impl basic getters + setters

--- src/core/Loop.ts
+++ src/core/Loop.ts
@@ -8,6 +8,8 @@ export type UpdateFunction = (dt: number) => {}
 export default class Loop {
 	private is_constant_dt: boolean = false;
 	private constant_dt: number = 0.16;
+	// for tracking purpose only. will not effect the loop
+	private is_running: boolean; 
 
 	private prev_time: number;
 	private update_func: UpdateFunction;
@@ -22,6 +24,7 @@ export default class Loop {
 		this.prev_time = 0;
 		this.frame_id = null;
 		this.frame_count = 0;
+		this.is_running = false;
 	}
 
 	private getDeltaTime(current_time: number) {
@@ -39,11 +42,13 @@ export default class Loop {
 	}
 
 	run(): number {
+		this.is_running = true;
 		this.frame_id = window.requestAnimationFrame(this._update);
 		return this.frame_id;
 	}
 
 	pause() {
+		this.is_running = false;
 		if (this.frame_id === null) return;
 		window.cancelAnimationFrame(this.frame_id);
 	}
@@ -54,9 +59,32 @@ export default class Loop {
 
 	step(dt: number = 0.16) {
 		if (dt <= 0) throw new Error("Delta time cannot be less than or equal to zero");
-		this.frame_count ++;
+		this.frame_count++;
 		this.update_func(dt);
 	}
+
+	//================================ Getters ================================
+
+	getFrameCounts() {
+		return this.frame_count;
+	}
+
+	isConstantDeltatime() {
+		return this.is_constant_dt;
+	}
+
+	getConstantDeltatime() {
+		return this.constant_dt;
+	}
+
+	getFrameId() {
+		return this.frame_id;
+	}
+
+	isRunning() {
+		return this.is_running;
+	}
+
 	//================================ Setters ================================
 	
 	setConstantDeltaTime(dt: number | null = null) {
@@ -70,4 +98,8 @@ export default class Loop {
 		return this;
 	}
 
+	setUpdateFunction(update_func: UpdateFunction) {
+		this.update_func = update_func;
+		return this;
+	}
 }
