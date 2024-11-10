3e8bc5a007d9db597f1ad3011346c4beeba43e90
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-10 14:04:05 -0500

    add: doc comments

--- src/interactor/Interactor.ts
+++ src/interactor/Interactor.ts
@@ -3,16 +3,37 @@ import Vec2 from "../utils/math/Vector";
 export default abstract class Interactor {
 	private is_locked: boolean = false;
 
-	abstract isBoundingBoxContainsPoint(pos: Vec2): boolean;
-	abstract isContainsPoint(pos: Vec2): boolean;
+	/**
+	* Check whether a point (x, y) is inside the body
+	**/
+	abstract isHovered(pos: Vec2): boolean;
+
+	/**
+	* Check whether a point (x, y) is inside the bounding box of the body.
+	* Used as a pre-check before performing any costly operation for precise checking
+	**/
+	abstract isBoundingBoxHovered(pos: Vec2): boolean;
+
+	/**
+	* Check whether the body is inside a selected area
+	* @param start: top-left corner position of the selected rectangle
+	* @param start: bottom-right corner position of the selected rectangle
+	**/
+	abstract isSelected(start: Vec2, end: Vec2): boolean;
 
 	//================================ Setters ================================
 
+	/**
+	* Turn the body into a locked state, unable to be selected
+	**/
 	lock() {
 		this.is_locked = true;
 		return this;
 	}
 
+	/**
+	* Turn the body back into the selectable state
+	**/
 	unlock() {
 		this.is_locked = false;
 		return this;
@@ -20,6 +41,9 @@ export default abstract class Interactor {
 
 	//================================ Getters ================================
 
+	/**
+	* Is the body selectable
+	**/
 	isLocked() {
 		return this.is_locked;
 	}
