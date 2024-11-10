314f09d23224e08e213c4ae8cbd4c0bc2b0e411e
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-10 13:54:51 -0500

    impl: basic constructor

--- src/interactor/PointMassInteractor.ts
+++ src/interactor/PointMassInteractor.ts
@@ -2,8 +2,13 @@ import PointMass from "../core-physic/PointMass";
 import Vec2 from "../utils/math/Vector";
 import Interactor from "./Interactor";
 
-export default class PointMassInteractor implements Interactor {
+export default class PointMassInteractor extends Interactor {
 	protected pointmass: PointMass;
+
+	constructor(pointmass: PointMass) {
+		super();
+		this.pointmass = pointmass;
+	}
 
     isBoundingBoxContainsPoint(pos: Vec2): boolean {
         throw new Error("Method not implemented.");
--- src/interactor/RigidConstraintInteractor.ts
+++ src/interactor/RigidConstraintInteractor.ts
@@ -2,8 +2,13 @@ import RigidConstraint from "../core-physic/RigidConstraint";
 import Vec2 from "../utils/math/Vector";
 import Interactor from "./Interactor";
 
-export default class RigidConstraintInteractor implements Interactor {
+export default class RigidConstraintInteractor extends Interactor {
 	protected rigid_constraint: RigidConstraint;
+
+	constructor(rigid_constraint: RigidConstraint) {
+		super();
+		this.rigid_constraint = rigid_constraint;
+	}
 
     isBoundingBoxContainsPoint(pos: Vec2): boolean {
         throw new Error("Method not implemented.");
