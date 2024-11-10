fbef5500738f3371e568cd22077beed7752a1431
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-09 23:25:47 -0500

    include substeps directly inside loop manager

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ export default function App() {
 			width: window.innerWidth,
 			height: window.innerHeight
 		});
+		const ctx = canvas.getContext();
+
 		// TODO: multiple sub steps dealing with visualization
 		// For now: < 20000 is recommended. Too many sub steps may cause visual anormally
 		const gravity = vec2(0, 9.8);
@@ -23,43 +25,38 @@ export default function App() {
 		const p2 = new PointMass().setPosition(Vec2.right(100)).setConstantAcceleration(gravity);
 		const p3 = new PointMass().setPosition(Vec2.right(150)).setConstantAcceleration(gravity);
 		const p4 = new PointMass().setPosition(Vec2.right(175)).setConstantAcceleration(gravity);
-
 		p2.renderer.position.setRadius(10).setFillColor('orange');
 		p2.renderer.velocity.enable();
 
 		const d1 = new RigidConstraint(p1, p2);
 		const d2 = new RigidConstraint(p2, p3);
 		const d3 = new RigidConstraint(p3, p4);
-
 		d1.renderer.stress.enable();
 
-		const steps = 20000.0; 
-		const loop = new Loop((dt: number) => {
-			const ctx = canvas.getContext();
-			if (!ctx) return;
-			dt /= steps;
+		const update = (dt: number, _: number) => {
+			p1.update(dt);
+			p2.update(dt);
+			p3.update(dt);
+			p4.update(dt);
 
-			for (let i = 0; i < steps; i++) {
-				p1.update(dt);
-				p2.update(dt);
-				p3.update(dt);
-				p4.update(dt);
+			d1.update(dt);
+			d2.update(dt);
+			d3.update(dt);
+		}; 
 
-				d1.update(dt);
-				d2.update(dt);
-				d3.update(dt);
-			}
-
+		const render = (_: number, sub_steps: number) => {
 			canvas.clear();
-			// TODO: pass in dt to make visualization consistant
-			d1.renderer.draw(ctx, steps);
-			d2.renderer.draw(ctx, steps);
-			d3.renderer.draw(ctx, steps);
-			p1.renderer.draw(ctx, steps);
-			p2.renderer.draw(ctx, steps);
-			p3.renderer.draw(ctx, steps);
-			p4.renderer.draw(ctx, steps);
-		});
+			d1.renderer.draw(ctx, sub_steps);
+			d2.renderer.draw(ctx, sub_steps);
+			d3.renderer.draw(ctx, sub_steps);
+
+			p1.renderer.draw(ctx, sub_steps);
+			p2.renderer.draw(ctx, sub_steps);
+			p3.renderer.draw(ctx, sub_steps);
+			p4.renderer.draw(ctx, sub_steps);
+		}
+
+		const loop = new Loop(update, render, { sub_steps: 1000 });
 
 		setLoop(loop);
 		loop.run();
