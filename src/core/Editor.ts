280cbeefdeb8d333f00b0bb0bd10eda53e9ef2fd
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-11 13:56:14 -0500

    use EditorRenderer

--- src/core/Editor.ts
+++ src/core/Editor.ts
@@ -1,3 +1,4 @@
+import EditorRenderer from "../renderer/EditorRenderer";
 import Vec2 from "../utils/math/Vector";
 import Canvas from "./Canvas";
 
@@ -14,6 +15,8 @@ export default class Editor {
 	private is_mouse_down: boolean;
 	private mouse_start_pos: Vec2;
 
+	public readonly renderer: EditorRenderer;
+
 	constructor(canvas: Canvas, {
 		drag_threshold = 5,
 	}: EditorParams = {}) {
@@ -21,17 +24,21 @@ export default class Editor {
 		this.drag_threshold = drag_threshold;
 		this.is_mouse_down = false;
 		this.mouse_start_pos = Vec2.zero();
+		this.setupMouseEvent();
+		this.renderer = new EditorRenderer(this);
+	}
 
-		canvas.onMouseDown((_: MouseEvent) => {
+	setupMouseEvent() {
+		this.canvas.onMouseDown((_: MouseEvent) => {
 			if (this.is_mouse_down) return;
 			this.is_mouse_down = true;
-			this.mouse_start_pos = canvas.getMousePosition();
+			this.mouse_start_pos = this.canvas.getMousePosition();
 		});
 
-		canvas.onMouseUp((_: MouseEvent) => {
+		this.canvas.onMouseUp((_: MouseEvent) => {
 			if (!this.is_mouse_down) return;
 			this.is_mouse_down = false;
-			const mouse_curr_pos = canvas.getMousePosition()
+			const mouse_curr_pos = this.canvas.getMousePosition();
 			const diff = mouse_curr_pos.sub(this.mouse_start_pos).mag();
 			if (diff < this.drag_threshold) this.click(this.mouse_start_pos);
 			else this.drag(this.mouse_start_pos, mouse_curr_pos);
@@ -51,5 +58,13 @@ export default class Editor {
 		const mouse_curr_pos = this.canvas.getMousePosition();
 		const diff = mouse_curr_pos.sub(this.mouse_start_pos).mag();
 		return diff >= this.drag_threshold;
+	}
+
+	getMouseStartPosition() {
+		return this.mouse_start_pos;
+	}
+
+	getMouseCurrentPosition() {
+		return this.canvas.getMousePosition();
 	}
 }
