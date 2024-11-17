2aa2a37fcc3d6bf4fcbabb217516875c788f4dae
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-17 14:46:14 -0500

    declare abstract onClick & onDrag

--- src/editor/EditorMode.ts
+++ src/editor/EditorMode.ts
@@ -1,3 +1,7 @@
-export default abstract class EditorMode {
+import Vec2 from "../utils/math/Vector";
+import { MouseButton } from "./Editor";
 
+export default abstract class EditorMode {
+	abstract onClick(button: MouseButton, pos: Vec2): void;
+	abstract onDrag(button: MouseButton, pos: Vec2): void;
 }
