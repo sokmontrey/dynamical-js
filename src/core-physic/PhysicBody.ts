92da6d90e7aa96736219b11200ccf40a1dcbb55b
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-18 16:52:28 -0500

    remove create

--- src/core-physic/PhysicBody.ts
+++ src/core-physic/PhysicBody.ts
@@ -1,7 +1,5 @@
 import Interactor from "../interactor/Interactor";
 import Renderer from "../renderer/Renderer";
-import PointMass from "./PointMass";
-import RigidConstraint from "./RigidConstraint";
 
 export default interface PhysicBody {
 	renderer: Renderer;
@@ -12,14 +10,3 @@ export enum PhysicBodyType {
 	POINTMASS,
 	RIGID_CONSTRAINT,
 }
-
-export const create = (type: PhysicBodyType, params: any) => {
-	switch(type) {
-	case PhysicBodyType.POINTMASS:
-		return new PointMass(params);
-	case PhysicBodyType.RIGID_CONSTRAINT:
-		return new RigidConstraint(params.pointmass1, params.pointmass2, params);
-	default:
-		throw new Error("Undefined Physic Body Type");
-	}
-};
