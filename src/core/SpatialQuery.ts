ad3906047f41cf287f5627f0eb47cb75ec7e55de
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-21 09:59:57 -0500

    use new Quadtree

--- src/core/SpatialQuery.ts
+++ src/core/SpatialQuery.ts
@@ -1,8 +1,8 @@
 import PointMass from "../core-physic/PointMass";
 import RigidConstraint from "../core-physic/RigidConstraint";
-import Point from "../quadtree/Point";
-import Quadtree from "../quadtree/Quadtree";
+import BoundingBox from "../utils/math/BoundingBox";
 import Vec2, { vec2 } from "../utils/math/Vector";
+import Quadtree from "./Quadtree";
 
 type Constraint = RigidConstraint;
 
@@ -16,7 +16,7 @@ export default class SpatialQuery {
 
 	private center: Vec2;
 	private half_dim: Vec2;
-	private pointmass_quadtree!: Quadtree<PointMass>;
+	private quadtree!: Quadtree;
 
 	constructor(width: number, height: number) {
 		this.body_hierarchy = {
@@ -25,7 +25,7 @@ export default class SpatialQuery {
 		};
 		this.center = Vec2.zero();
 		this.half_dim = vec2(width, height).div(2);
-		this.resetPointMassQuadtree();
+		this.resetQuadtree();
 	}
 
 	addPointMass(pointmass: PointMass) {
@@ -40,30 +40,29 @@ export default class SpatialQuery {
 	* Must be called before any query
 	**/
 	update() {
-		this.updatePointMassQuadtree();
+		this.updateQuadtree();
 	}
 
-    updatePointMassQuadtree() {
-		this.resetPointMassQuadtree();
-		for(const pm of this.body_hierarchy.pointmasses) {
-			const point = new Point<PointMass>(pm.getPosition(), pm);
-			this.pointmass_quadtree.insert(point);
+	updateQuadtree() {
+		this.resetQuadtree();
+		for (const pm of this.body_hierarchy.pointmasses) {
+			this.quadtree.insert(pm);
 		}
-    }
+	}
 
-    private resetPointMassQuadtree() {
-        this.pointmass_quadtree = new Quadtree<PointMass>(this.center, this.half_dim);
-    }
+	private resetQuadtree() {
+		this.quadtree = new Quadtree(this.center, this.half_dim);
+	}
 
-    removePointMass(pointmass: PointMass) {
-        throw new Error("Method not implemented.");
-    }
+	removePointMass(pointmass: PointMass) {
+		throw new Error("Method not implemented.");
+	}
 
 	pickPointMass(pos: Vec2) {
-        throw new Error("Method not implemented.");
+		throw new Error("Method not implemented.");
 	}
 
-	selectPointMasses(lower: Vec2, upper: Vec2) {
-        throw new Error("Method not implemented.");
+	selectPointMasses(lower: Vec2, upper: Vec2): PointMass[] {
+		return this.quadtree.queryRange(BoundingBox.fromUpperAndLower(lower, upper));
 	}
 }
