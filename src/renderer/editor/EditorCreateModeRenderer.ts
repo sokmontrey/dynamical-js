29e44c9fb5d26fa6161894ef8e7b140594dedb72
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-24 23:37:17 -0500

    simplify constructor params

--- src/renderer/editor/EditorCreateModeRenderer.ts
+++
@@ -1,7 +0,0 @@
-import Renderer from "../Renderer";
-
-export default class EditorCreateModeRenderer implements Renderer {
-    draw(ctx: CanvasRenderingContext2D, steps: number): Renderer {
-		return this;
-    }
-}
--- src/renderer/editor/EditorMoveModeRenderer.ts
+++ src/renderer/editor/EditorMoveModeRenderer.ts
@@ -4,11 +4,11 @@ import ShapeStyle from "../../style/ShapeStyle";
 import Renderer from "../Renderer";
 
 export default class EditorMoveModeRenderer implements Renderer {
-	private editor_move_mode: EditorMoveMode;
+	private editor_mode: EditorMoveMode;
 	public readonly drag_rectangle: ShapeStyle;
 
-	constructor(editor_move_mode: EditorMoveMode) {
-		this.editor_move_mode = editor_move_mode;
+	constructor(editor_mode: EditorMoveMode) {
+		this.editor_mode = editor_mode;
 		this.drag_rectangle = new ShapeStyle()
 			.setFillColor("#4287f522")
 			.setStrokeColor("#4287f5")
@@ -18,9 +18,9 @@ export default class EditorMoveModeRenderer implements Renderer {
 
 	drawDraggingRectangle(ctx: CanvasRenderingContext2D) {
 		if (!this.drag_rectangle.isEnable()) return;
-		if (!this.editor_move_mode.isDragging()) return;
-		const start = this.editor_move_mode.getMouseStartPosition();
-		const end = this.editor_move_mode.getMouseCurrentPosition();
+		if (!this.editor_mode.isDragging()) return;
+		const start = this.editor_mode.getMouseStartPosition();
+		const end = this.editor_mode.getMouseCurrentPosition();
 		const dim = end.sub(start);
 		Draw.rectangle(ctx, start, dim, this.drag_rectangle);
 	}
