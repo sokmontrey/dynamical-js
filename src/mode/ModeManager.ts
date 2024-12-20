7a31916c55443d586dbcc97ab20a008f21a0fd4e
Sokmontrey <sythatsokmontrey@gmail.com>
2024-12-20 12:04:26 -0500

    refactor mode switching mechanism
    - Add observer for listening to mode changes
    - Use a more general enum for mode type
    - Add a simple mode indicator in App.tsx

--- src/mode/ModeManager.ts
+++ src/mode/ModeManager.ts
@@ -4,28 +4,30 @@ import MoveMode from "./MoveMode.ts";
 import Mode from "./Mode.ts";
 import Editor, {MouseButton} from "../core/Editor.ts";
 
-export enum CreateMode {
-    POINTMASS = "PointMass",
-    RIGID_CONSTRAINT = "RigidConstraint",
+export enum ModeType {
+    MOVE = "Move",
+    CREATE_POINTMASS = "CreatePointMass",
+    CREATE_RIGID_CONSTRAINT = "CreateRigidConstraint",
 }
 
 export default class ModeManager {
+    private current_mode_type!: ModeType;
     private current_mode!: Mode;
     private readonly editor: Editor;
+    private on_mode_change: (mode: ModeType) => void = () => {};
 
     constructor(editor: Editor) {
         this.editor = editor;
         this.toMoveMode();
-        // this.toCreateMode(CreateMode.POINTMASS);
     }
 
-    public toCreateMode(create_mode: CreateMode) {
+    public toCreateMode(create_mode: ModeType) {
         switch (create_mode) {
-            case CreateMode.POINTMASS:
-                this.toMode(new CreatePointMassMode());
+            case ModeType.CREATE_POINTMASS:
+                this.toMode(new CreatePointMassMode(), ModeType.CREATE_POINTMASS);
                 break;
-            case CreateMode.RIGID_CONSTRAINT:
-                this.toMode(new CreateRigidConstraintMode());
+            case ModeType.CREATE_RIGID_CONSTRAINT:
+                this.toMode(new CreateRigidConstraintMode(), ModeType.CREATE_RIGID_CONSTRAINT);
                 break;
             default:
                 throw new Error("Invalid create mode");
@@ -33,13 +35,15 @@ export default class ModeManager {
     }
 
     public toMoveMode() {
-        this.toMode(new MoveMode());
+        this.toMode(new MoveMode(), ModeType.MOVE);
     }
 
-    public toMode(mode: Mode) {
+    public toMode(mode: Mode, mode_type: ModeType) {
         this.current_mode = mode;
+        this.current_mode_type = mode_type;
         this.current_mode.setModeManager(this);
         this.current_mode.setEditor(this.editor);
+        this.on_mode_change?.(mode_type);
     }
 
     public onMouseMove() {
@@ -61,6 +65,18 @@ export default class ModeManager {
     public getCurrentMode(): Mode {
         return this.current_mode;
     }
+
+    public getCurrentModeType(): ModeType {
+        return this.current_mode_type;
+    }
+
+    public getCreateModeTypes(): ModeType[] {
+        return [ModeType.CREATE_POINTMASS, ModeType.CREATE_RIGID_CONSTRAINT];
+    }  
+
+    public onModeChange(callback: (mode: ModeType) => void) {
+        this.on_mode_change = callback;
+    }   
 
     reset() {
         this.toMoveMode();
