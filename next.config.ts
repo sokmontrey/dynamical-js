87ac5c32fe947cb94a63864ca483381aa2ab70e5
Sokmontrey <sythatsokmontrey@gmail.com>
2024-12-30 21:19:54 -0500

    change to export mode

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: "dist",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
